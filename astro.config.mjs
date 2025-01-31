e67cd9cd86a8d70aac5de0ce8b26ec9c5a0afeba
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2025-01-31 16:56:01 +0100

    netlify adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://guillaumewebdev.com",
-  integration: [sitemap()],
+  output: "server",
+  adapter: netlify(),
 });
